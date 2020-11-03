3e646071e5c5f062d4c3d74e2841906901d77d55
Chengjie Yang <cyang58@cs.binghamton.edu>
2020-11-03 14:33:35 -0500

    completing lab9 + carry over prj3

--- submit/lab9/exercises/parity/parity.s
+++ submit/lab9/exercises/parity/parity.s
@@ -4,5 +4,11 @@
 get_parity:
 
 	#@TODO: add code here to set eax to 1 iff edi has even parity
+	test	%eax, %eax	#set flag
+	jpe	if_even_par
+	xorl	%eax, %eax
 	ret
 	
+if_even_par:
+	movl	$1, %eax
+	ret
